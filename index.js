5b8bed6b33116f34850f7553b834687f8660ccc5
Drumil Bhati <64512394+drumilbhati@users.noreply.github.com>
2025-07-18 04:53:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+const cron = require('node-cron');
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: true,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+});
+
+const mailOptions = {
+    from: process.env.MAIL_USER,
+    to: process.env.MAIL_TO,
+    subject: 'Today\'s study tracking',
+    text: 'This is a reminder to track your study progress for today.\nFill your study log at ' + process.env.STUDY_LOG_URL,
+    html: `This is a reminder to track your study progress for today.<br>
+           Fill your study log at <a href="${process.env.STUDY_LOG_URL}">${process.env.STUDY_LOG_URL}</a>`,
+};
+
+cron.schedule('23 10 * * *', () => {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log('Error occurred while sending email:', error);
+        }
+        console.log('Email sent successfully:', info.response);
+    });
+});
